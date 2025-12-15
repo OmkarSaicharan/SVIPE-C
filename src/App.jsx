b200274363322c2bd8e540ac4b3e59e6a65b7226
omkar <161125388+OmkarSaicharan@users.noreply.github.com>
2025-12-15 22:10:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -632,7 +632,7 @@ const App = () => {
             </div>
              <div className="portfolio-item">
               <img
-                src="https://images.unsplash.com/photo-1488646953014-85cb44e25828?auto=format&fit=crop&w=800&q=80"
+                src="https://images.unsplash.com/photo-1488190211105-8b0e65b80b4e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 alt="UPSC Book Library"
                 className="portfolio-img"
               />
@@ -642,7 +642,7 @@ const App = () => {
                   All Topics and Content of upsc preparation in one place. All related links downloads videos and materials in one click.
                 </p>
                 <a
-                  href="https://omkarsaicharan.github.io/Bookmatte/"
+                  href="https://upsclibrary.vercel.app/"
                   target="_blank"
                   rel="noreferrer"
                   className="btn btn-outline"
